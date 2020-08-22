4c64285b4c1fb0c25fd97a0cc36f115a54a3c584
신 동훈 <huipulco1@naver.com>
2020-08-22 23:56:58 +0000

    first

---
+++ animated.js
@@ -0,0 +1,99 @@
+function isElementUnderBottom(elem, triggerDiff) {
+  const { top } = elem.getBoundingClientRect(); //elem.getBoundingClientRect().top
+  const { innerHeight } = window;//window.innerHeight
+  return top > innerHeight + (triggerDiff || 0);
+}
+
+function isElementEntirelyShow(elem) {
+  const { bottom } = elem.getBoundingClientRect(); //elem.getBoundingClientRect().top
+  const { innerHeight } = window;//window.innerHeight
+  return bottom> innerHeight ;
+}
+
+function handleScrollUpFadeIn() {
+  const elems = document.querySelectorAll('.up-on-scroll');
+  elems.forEach(elem => {
+    if (isElementUnderBottom(elem, -20)) {
+	  fadeIn(elem);
+      //elem.style.opacity = "0";
+      //elem.style.transform = 'translateY(70px)';
+    } else {
+      elem.style.opacity = "1";
+      elem.style.transform = 'translateY(0px)';
+    }
+  })
+ 
+}
+
+function handle1ScrollOnlyFadeIn() {
+  const elems = document.querySelectorAll('.fade-in');
+  elems.forEach(elem => {
+    if (isElementUnderBottom(elem,-20)) {
+      fadeInOnly(elem);
+      //elem.style.transform = 'translateY(70px)';
+    } else {
+      elem.style.opacity = "1";
+    }
+  })
+ 
+}
+
+function fadeIn(element) {
+    var opacity = 0;
+	var translateY =70;
+    function increase () {
+        opacity += 0.05;
+		translateY -=1.9;//값이 클수록 빠르게 올라옴
+        if (opacity >= 1 && translateY<=0){
+            // complete
+            element.style.opacity = 1;
+			element.style.transform = 'translateY(0px)';
+            return true;
+        }else if(opacity >= 1&& translateY>0){//translateY가 0보다 큰 경우
+			element.style.transform = 'translateY('+translateY+'px)';
+			requestAnimationFrame(increase);
+		}else if(opacity < 1 && translateY<=0){// opacity가 1보다 작은 경우
+			element.style.opacity = opacity;
+			requestAnimationFrame(increase);
+		}else{
+			element.style.opacity = opacity;
+			element.style.transform = 'translateY('+translateY+'px)';
+        	requestAnimationFrame(increase);
+		}
+        
+    }
+    increase();
+}
+function fadeInOnly(element) {
+    var opacity = 0;
+    function increase () {
+        opacity += 0.05;
+        if (opacity >= 1 ){
+            // complete
+            element.style.opacity = 1;
+            return true;}
+   		else if(opacity < 1){// opacity가 1보다 작은 경우
+			element.style.opacity = opacity;
+			requestAnimationFrame(increase);
+		}
+        
+    }
+    increase();
+}
+
+window.addEventListener('scroll', handleScrollUpFadeIn);
+window.addEventListener('scroll', handle1ScrollOnlyFadeIn );
+
+
+/*flex-direction 애니매이션 코드 https://codepen.io/osublake/pen/eJGrPN*/
+console.clear();
+
+
+
+
+
+
+function textFill(id){
+	let spanEl=document.getElementsById(id);
+	spanEl.style.color='black';
+}
